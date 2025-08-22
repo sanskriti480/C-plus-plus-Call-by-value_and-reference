b02bf238239400e92765fdc52942decda48aec5f
sanskriti480 <sanskriti.ten@gmail.com>
2025-08-22 11:13:00 +0530

    Create Exp 10 Call by value.cpp

---
+++ Exp 10 Call by value.cpp
@@ -0,0 +1,30 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 10
+
+#include <iostream>
+using namespace std;
+
+//Call by value
+int swap(int x,int y)
+{
+    int swap;
+    swap=x;
+    x=y;
+    y=swap;
+    return x,y;
+}
+int main()
+{
+    int a=5,b=2;
+    swap(a,b); //passing value to function
+    cout<<"Value of a is: "<<a<<endl;
+    cout<<"Value of b is: "<<b<<endl;
+    return 0;
+}
+
+//Output
+
+//Value of a is: 5
+//Value of b is: 2
